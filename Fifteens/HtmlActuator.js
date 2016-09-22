7b20e6f60a616c9923153715a1d3df33d8ea5ab8
OverLucker <lucker1005000@gmail.com>
2016-09-22 20:31:56 +0300

    Added Win Game Mess & Moves

--- Fifteens/HtmlActuator.js
+++ Fifteens/HtmlActuator.js
@@ -12,6 +12,8 @@ Actuator.movetile = function(x1, y1, x2, y2)
 
 Actuator.actuate = function (game)
 {
+    var mmtext = document.querySelector("#moves_mixed")
+    mmtext.dataset.content = game.mixmoves;
 	for (i = 0; i < game.grid.X; ++i)
 	{
 		for (j = 0; j < game.grid.Y; ++j)
@@ -21,33 +23,41 @@ Actuator.actuate = function (game)
 			tile.setAttribute("class", cls.join(" "));
 		}
 	}
+    this.updatemoves (Game.moves);
 }
 
 Actuator.updatemoves = function(moves)
 {
-	var class_name = ["moves_done"];
-	var scores = document.getElementsByClassName(class_name.join(" "))[0];
-	scores.textContent = moves;
+    var scorestext = document.querySelector("#moves_done");
+    scorestext.dataset.content = moves;
 }
 
 window.addEventListener("keydown", function (event) {
 	var action = 
 	{
+        13 : 13,
 		37 : { dx : 1, dy : 0}, // left
 		38 : { dx : 0, dy : 1}, // up
 		39 : { dx : -1, dy : 0 }, // right
 		40 : { dx : 0, dy : -1 },  // down
-        75 : { dx : 1, dy : 0}, // Vim up
-        76 : { dx : 0, dy : 1}, // Vim right
-        74 : { dx : -1, dy : 0 }, // Vim down
-        72 : { dx : 0, dy : -1 }, // Vim left        
-        87 : { dx : 1, dy : 0}, // W
-        68 : { dx : 0, dy : 1}, // D
-        83 : { dx : -1, dy : 0 }, // S
-        65 : { dx : 0, dy : -1 }  // A
+        // 75 : { dx : 1, dy : 0}, // Vim up
+        // 76 : { dx : 0, dy : 1}, // Vim right
+        // 74 : { dx : -1, dy : 0 }, // Vim down
+        // 72 : { dx : 0, dy : -1 }, // Vim left        
+        // 87 : { dx : 1, dy : 0}, // W
+        // 68 : { dx : 0, dy : 1}, // D
+        // 83 : { dx : -1, dy : 0 }, // S
+        // 65 : { dx : 0, dy : -1 }  // A
 	}
 	if (action [event.which] !== undefined)
 	{
+        if (action[event.which] == 13)
+        {
+            Game.make_new();
+            Actuator.actuate(Game);
+            Mess_State('none');
+        }
+        
 		var ppos = { X : Game.tileZ.X, Y : Game.tileZ.Y};
 		if (Game.movetile(action[event.which].dx, action[event.which].dy))
 			return;
@@ -59,9 +69,7 @@ window.addEventListener("keydown", function (event) {
 		if (Game.check() == 1)
 		{
             Mess_State('block');
-			Game.make_new();
-			Actuator.actuate(Game);
-			Actuator.updatemoves(Game.moves);
+			
 		}
 	}
 });
@@ -78,7 +86,8 @@ window.onload = function () {
     btn_new_game = document.getElementById('new_game');
     btn_new_game.onclick = function() {
         Game.make_new();
-        Actuator.actuate(Game);
+		Actuator.actuate(Game);
+		Actuator.updatemoves(Game.moves);
         Mess_State('none');
     }
 }
